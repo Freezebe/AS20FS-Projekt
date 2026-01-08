158defb38012ec463e855f0c8bbb9021d04c950f
Freezebe <isac.lagerstrom@gmail.com>
2026-01-08 11:51:15 +0100

    Finskrivningar och några kategorier tillagda

--- LSD-Script.sh
+++ LSD-Script.sh
@@ -4,6 +4,10 @@
 #
 echo "Startar LinuxStartupDokumenterare...."
 echo ""
+echo "--===DatorInfo===--"
 echo "$OSTYPE"
 echo ""
+echo "--===Aplikationer===--"
+echo ""
+echo "--===NätverksInfo===--"
 curl ip-api.com | grep "query"
