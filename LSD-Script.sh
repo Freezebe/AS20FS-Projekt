227b488d541fb98e2c50ded5569418fb02b5b1d7
Freezebe <isac.lagerstrom@gmail.com>
2026-01-15 13:26:26 +0100

    Utökade Datorinformationen och skrev insturktioner för att göra autostart möjligt.

--- LSD-Script.sh
+++ LSD-Script.sh
@@ -5,6 +5,11 @@
 echo "Startar LinuxStartupDokumenterare...."
 echo ""
 OS="$OSTYPE"
+if ! command -v bash &> /dev/null; then
+    echo "Fel upptäkt! BASH är inte installerat"
+    echo "Stänger av skriptet!"
+fi
+
 if [[  "$OS" != "linux-gnu"* ]]; then
     echo "Operativsystemet är inte Linux-gnu baserat, avbryter skriptet..."
     exit 
@@ -20,9 +25,12 @@ whoami
 echo ""
 echo "Total Lagringsmängd"
 cat /proc/meminfo | grep "MemTotal:"
+echo ""
 echo "Ledig Lagringsmängd:"
+cat /proc/meminfo | grep "MemAvailable:"
 echo "--===Aplikationer===--"
 echo "Aktiva Tjänster:"
 ps
+echo ""
 echo "--===NätverksInfo===--"
 curl ip-api.com | grep "query"
